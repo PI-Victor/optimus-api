fa7dcaa42b1d39bc5880d0f9e616a175c886cf26
Victor Palade <victor@cloudflavor.io>
2018-12-26 18:36:06 +0100

    api: add additional handlers and routes.

--- pkg/apis/v1alpha1/handlers.go
+++ pkg/apis/v1alpha1/handlers.go
@@ -2,7 +2,10 @@ package v1alpha1
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/cloudflavor/optimus-api/pkg/apis"
 )
@@ -11,20 +14,51 @@ func index(w http.ResponseWriter, r *http.Request) {
 	availableAPIVersions := apis.APIVersions
 
 	json.NewEncoder(w).Encode(availableAPIVersions)
+	w.WriteHeader(http.StatusOK)
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
+func createUser(w http.ResponseWriter, r *http.Request) {
+	httpResponseCode := http.StatusOK
 
+	user := &User{}
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logrus.Errorf("Couldn't parse request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(b, user)
+	if err != nil {
+		logrus.Errorf("Couldn't unmarshall request body: %s", err)
+		httpResponseCode = http.StatusInternalServerError
+		w.WriteHeader(httpResponseCode)
+		return
+	}
+
+	w.WriteHeader(httpResponseCode)
 }
 
 func findUserByID(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(http.StatusOK)
 }
 
 func createNamespace(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(http.StatusOK)
 }
 
-func getNamespaces(w http.ResponseWriter, r *http.Request) {
+func getNamespaceByID(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
 
+func createJob(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func getJobByID(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func callGitHubWebHook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
--- pkg/apis/v1alpha1/routes.go
+++ pkg/apis/v1alpha1/routes.go
@@ -26,7 +26,43 @@ var Routes = []Route{
 	{
 		Name:    "users",
 		Pattern: fmt.Sprintf("/%s/%s", APIVersion, "users"),
+		Method:  http.MethodPost,
+		Handler: createUser,
+	},
+	{
+		Name:    "users",
+		Pattern: fmt.Sprintf("/%s/%s/{id:[0-9]+}", APIVersion, "users"),
 		Method:  http.MethodGet,
-		Handler: getUser,
+		Handler: findUserByID,
+	},
+	{
+		Name:    "namespaces",
+		Pattern: fmt.Sprintf("/%s/%s", APIVersion, "namespaces"),
+		Method:  http.MethodPost,
+		Handler: createNamespace,
+	},
+	{
+		Name:    "namespaces",
+		Pattern: fmt.Sprintf("/%s/%s/{id:[0-9]+}", APIVersion, "namespaces"),
+		Method:  http.MethodGet,
+		Handler: getNamespaceByID,
+	},
+	{
+		Name:    "jobs",
+		Pattern: fmt.Sprintf("/%s/%s", APIVersion, "jobs"),
+		Method:  http.MethodGet,
+		Handler: createJob,
+	},
+	{
+		Name:    "jobs",
+		Pattern: fmt.Sprintf("/%s/%s/{id:[0-9]+}", APIVersion, "jobs"),
+		Method:  http.MethodPost,
+		Handler: getJobByID,
+	},
+	{
+		Name:    "webhooks",
+		Pattern: fmt.Sprintf("/%s/webhooks/%s", APIVersion, "github"),
+		Method:  http.MethodPost,
+		Handler: callGitHubWebHook,
 	},
 }
