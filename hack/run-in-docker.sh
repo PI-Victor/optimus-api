8d900903e2771d04f3a946f7f9bfabcf5373854c
Victor Palade <victor@cloudflavor.io>
2018-12-29 01:02:26 +0100

    scripts: add testing functionality
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- hack/run-in-docker.sh
+++ hack/run-in-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "utility needed: go get -u github.com/golang/dep/cmd/dep"
-echo "OPTIMUS_SSL_CERT_PATH=\"/go/src/github.com/cloudflavor/optimus-api/certificate.pem\" OPTIMUS_SSL_CERT_KEY_PATH=\"/go/src/github.com/cloudflavor/optimus-api/key.pem\" go run cmd/api/main.go"
+echo "OPTIMUS_LOG_LEVEL=7 OPTIMUS_SSL_CERT_PATH=\"/go/src/github.com/cloudflavor/optimus-api/certificate.pem\" OPTIMUS_SSL_CERT_KEY_PATH=\"/go/src/github.com/cloudflavor/optimus-api/key.pem\" go run cmd/api/main.go"
 echo "openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem"
-docker run --rm --name optimus-app -ti --link optimus-db -v $GOPATH:/go -w /go/src/github.com/cloudflavor/optimus-api -p 8000:8000 golang:1.10-stretch /bin/bash
+echo "go get github.com/smartystreets/goconvey"
+docker run --rm --name optimus-app -ti --link optimus-db -v $GOPATH:/go -w /go/src/github.com/cloudflavor/optimus-api -p 8000:8000 -p 8080:8080 golang:1.10-stretch /bin/bash
